3d04e3a54d49352eaaa8a32bc1c5199cc2591a55
mikinano7 <halx25@gmail.com>
2016-07-21 23:44:58 +0900

    プレーヤ実装

--- view.js
+++ view.js
@@ -3,11 +3,12 @@ const Config = require('config');
 
 var $ = jQuery = require("./jquery-2.1.4.min.js");
 
+var orig_player;
+var is_playing;
+var current_music;
+
 function init(){
-    SC.initialize({
-        client_id: Config.client_id,
-        redirect_uri: "http://example.com/callback.html"
-    });
+    SC.initialize({client_id: Config.client_id});
 
     SC.get('/tracks', {
         q: 'てーきゅう+metal'
@@ -19,5 +20,42 @@ function init(){
 function set_artwork(track) {
     var artwork_url = track.artwork_url;
     if (track.artwork_url == null) artwork_url = './resources/blank.png';
-    $('#track_info').prepend('<img src="'+artwork_url+'" />');
+    $('#track_list').prepend('<div id="'+track.id+'"></div>');
+
+    var trackElement = $('#'+track.id);
+    trackElement.prepend('<p>'+track.title+'</p>');
+    trackElement.prepend('<img src="'+artwork_url+'" class="change" />')
 }
+
+function play(id) {
+    current_music = id;
+    is_playing = true;
+    orig_player.play();
+}
+
+function pause(id) {
+    is_playing = false;
+    orig_player.pause();
+}
+
+$(document).on('click', '#play', function(){
+    play(current_music);
+});
+
+$(document).on('click', '#pause', function(){
+    pause(current_music);
+});
+
+$(document).on('click', '.change', function(){
+    var id = $(this).parent().attr('id');
+
+    SC.stream('tracks/'+id).then(function(player){
+        if (current_music == id) {
+            if (is_playing) pause(id);
+            else play(id);
+        } else {
+            orig_player = player;
+            play(id);
+        }
+    });
+});
